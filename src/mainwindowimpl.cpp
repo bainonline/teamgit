77cef9d159552eeadc52345aafb8b0cd92d50f03
Abhijit Bhopatkar <bain@devslashzero.com>
2008-09-15 16:08:48 +0530

    Show the current repo path in title bar

--- src/mainwindowimpl.cpp
+++ src/mainwindowimpl.cpp
@@ -343,6 +343,7 @@ void MainWindowImpl::initSlot()
 void MainWindowImpl::refresh()
 {
 	gt->git->setWorkingDir(gSettings->teamGitWorkingDir + gSettings->currProjectPath);
+	setWindowTitle("TeamGit - " + gSettings->teamGitWorkingDir);
 	commit_author->clear();
 	commit_date->clear();
 	commit_log->clear();
