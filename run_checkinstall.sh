6082d0a73cfedc6b38adbbf4b6cc98929f052a29
Abhijit Bhopatkar <bain@devslashzero.com>
2008-09-19 20:18:58 +0530

    Don't depend on qt 4.4.1 we can do with just 4.4

--- run_checkinstall.sh
+++ run_checkinstall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo checkinstall --requires="libqt4-core \(\>\=4.4.1\)" --maintainer="bain@devslashzero.com"
+sudo checkinstall --requires="libqt4-core \(\>\=4.4\)" --maintainer="bain@devslashzero.com"
