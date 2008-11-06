71777fa63c0718101ca0e7d3a436a8bd3922e78b
Abhijit Bhopatkar <bain@devslashzero.com>
2008-11-06 19:11:20 +0530

    Changed key name in write-changlog

--- tools/write-changelog.sh
+++ tools/write-changelog.sh
@@ -3,4 +3,4 @@
 file=debian/changelog
 echo "teamgit ($1-1) intrepid; urgency=low" > $file
 git shortlog >> $file
-echo \ -- Abhijit Bhopatkar \(My launchpadgpg key\) \<bain@devslashzero.com\> `date -R` >> $file
+echo \ -- Abhijit Bhopatkar \(My launchpad gpg key\) \<bain@devslashzero.com\> `date -R` >> $file
