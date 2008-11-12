060ed26bdbb03a9cefd5893e6a3fb29c04008f60
Abhijit Bhopatkar <bain@devslashzero.com>
2008-11-13 00:10:31 +0530

    Removed unnecessary scripts for debian packagin

--- tools/create_deb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git archive  --format=tar --prefix=teamgit-$1/ v$1 | gzip > ../teamgit_$1.orig.tar.gz
-tools/write-changelog.sh $1
-debuild -S
--- tools/write-changelog.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-file=debian/changelog
-echo "teamgit ($1-1) intrepid; urgency=low" > $file
-git shortlog >> $file
-echo \ -- Abhijit Bhopatkar \(My launchpad gpg key\) \<bain@devslashzero.com\> `date -R` >> $file
