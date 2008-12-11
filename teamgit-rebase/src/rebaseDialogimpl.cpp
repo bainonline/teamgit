da8fe1c618f23192b3cd021236658901d38c3f0f
Abhijit Bhopatkar <bain@devslashzero.com>
2008-12-11 12:38:39 +0530

    rabse-interactive: Added rebase control file reading
    Signed-off-by: Abhijit Bhopatkar <bain@devslashzero.com>

--- teamgit-rebase/src/rebaseDialogimpl.cpp
+++ teamgit-rebase/src/rebaseDialogimpl.cpp
@@ -1,15 +1,46 @@
+
+#include <QString>
 #include "rebaseDialogimpl.h"
+
 //
 DialogImpl::DialogImpl( QWidget * parent, Qt::WFlags f) 
 	: QDialog(parent, f)
 {
 	setupUi(this);
+	
+	commitsListView->addColumn("action");
+	commitsListView->addColumn("commit");
+	
 	QStringList args = QCoreApplication::arguments ();
 	if(args.size() > 1) {
-		rebaseFile = new QFile(args[1]);
-	
+		openFile(args[1]);
 	} else {
 		//TODO: quit
+	}
+}
+
+void DialogImpl::openFile(QString file) 
+{
+	rebaseFile = new QFile(file);
+	if (!rebaseFile->open(QIODevice::ReadWrite | QIODevice::Text))
+		return;
+
+	while (!rebaseFile->atEnd()) {
+		QByteArray line = rebaseFile->readLine();
+		QString lineString(line);
+		 processLine(lineString);
+	}
+}
+
+void DialogImpl::processLine(QString line)
+{
+	if(line.startsWith("#"))
+		return;
+	else {
+		QStringList lineItems = line.split(" ");
+		if(lineItems.size() > 2) {
+			new Q3ListViewItem(commitsListView,lineItems[0],lineItems[1]);
+		}
 	}
 }
 //
--- teamgit-rebase/src/rebaseDialogimpl.h
+++ teamgit-rebase/src/rebaseDialogimpl.h
@@ -9,10 +9,16 @@ class DialogImpl : public QDialog, public Ui::Dialog
 {
 Q_OBJECT
 private:
+
 	QFile *rebaseFile;
+	
+	void openFile(QString file);
+	void processLine(QString line);
 public:
 	DialogImpl( QWidget * parent = 0, Qt::WFlags f = 0 );
+	
 private slots:
+
 };
 #endif
 
